10ee0633d247db0f1a379f8a9befc66d8f40a3bf
jossrc <kuro.rc10@gmail.com>
2021-09-20 13:40:34 -0500

    Desarrollo de un list group

--- src/app/book/pages/book/book.component.ts
+++ src/app/book/pages/book/book.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book',
-  templateUrl: './book.component.html'
+  templateUrl: './book.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class BookComponent implements OnInit {
 
