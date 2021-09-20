3405348e955c4d75f4b578856fc98e57bec0aeed
jossrc <kuro.rc10@gmail.com>
2021-09-20 13:51:01 -0500

    Haciendo funcionar las rutas del navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     FooterComponent,
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     FooterComponent,
