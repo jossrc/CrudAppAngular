5072f50043121659e251829b084f33f74b0aa673
jossrc <kuro.rc10@gmail.com>
2021-09-20 15:33:18 -0500

    Creando tabla para la lista

--- src/app/book/pages/list/list.component.ts
+++ src/app/book/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../interfaces/book.interface';
 
 @Component({
   selector: 'app-list',
@@ -6,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  listOfBooks: Book[] = [
+    {
+      id: 1,
+      title: 'El mejor libro',
+      category: 'Autoayuda',
+      pages: 325
+    },
+    {
+      id: 2,
+      title: 'Pepito',
+      category: 'Terror',
+      pages: 521
+    },
+    {
+      id: 3,
+      title: 'Canción de Hielo y Fuego',
+      category: 'Fantasía',
+      pages: 1250
+    },
+    {
+      id: 4,
+      title: 'Harry Potter',
+      category: 'Fantasía',
+      pages: 670
+    },
+    {
+      id: 5,
+      title: 'Las Aventuras de Tintin',
+      category: 'Drama',
+      pages: 122
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  removeBookById(id: number) {
+    console.log(`Se eliminó el libro con id ${id}`);
   }
 
 }
