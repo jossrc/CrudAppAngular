76c0b8b3ee04c2f9cd09821bc46bc6bf121e85a7
jossrc <kuro.rc10@gmail.com>
2021-09-20 14:44:41 -0500

    Creando formulario para registrar

--- src/app/book/pages/add/add.component.ts
+++ src/app/book/pages/add/add.component.ts
@@ -2,7 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add',
-  templateUrl: './add.component.html'
+  templateUrl: './add.component.html',
+  styles: [
+    `
+    .bg-gray {
+      background: #F4F4F4;
+      border: none!important;
+      border-radius: 20px;
+    }    
+    `
+  ]
 })
 export class AddComponent implements OnInit {
 
