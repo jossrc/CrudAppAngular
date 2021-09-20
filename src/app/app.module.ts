ee8dd8cf4d7031904f04e07d1219fa9f732ad120
jossrc <kuro.rc10@gmail.com>
2021-09-20 13:15:37 -0500

    Importanto el rounting module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BookModule } from './book/book.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    BookModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
